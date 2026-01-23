fc6b201b88f97eade2ba1cd0de75dc2c885a3382
cr1pto <20729543+cr1pto@users.noreply.github.com>
2026-01-22 20:28:22 -0800

    Update project structure

--- src/be/afcapp_root/afcapp/models/base.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-# https://docs.sqlalchemy.org/en/20/orm/quickstart.html#declare-models
-class Base(DeclarativeBase):
-    pass
--- src/be/afcapp_root/afcapp/security/afc_security.py
+++
@@ -1,8 +0,0 @@
-from fastapi.security import OAuth2PasswordBearer
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-class AfcSecurity:
-    
-    
