0fdd1c28e1d5c4626c7fec621ac1482b4dd26d62
cr1pto <20729543+cr1pto@users.noreply.github.com>
2026-01-27 21:06:58 -0800

    rename

--- src/fe/app/layout.tsx
+++ src/fe/app/layout.tsx
@@ -1,8 +1,8 @@
-import Footer from "@/components/footer";
-import Navbar from "@/components/navbar";
 import type { Metadata } from "next";
 import { Barlow_Condensed, EB_Garamond, Libre_Baskerville, Lora, Merriweather, Noto_Serif, Playfair_Display, PT_Serif, Roboto, Noto_Sans, Work_Sans, Epilogue } from 'next/font/google';
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const roboto = Roboto({
   weight: ['100', '300', '400', '500', '700', '900'],
