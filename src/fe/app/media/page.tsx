3f5f8ef629c015f77dda0440803f7b907eec5963
Annika Budean <annikabudean@gmail.com>
2026-01-29 09:46:17 -0800

    Starting with media page

---
+++ src/fe/app/media/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Media() {    
+
+  return (
+    <div className="flex flex-col justify-center bg-white">
+        <div className="flex justify-center items-center h-50 bg-[url('../public/bible-vertical.jpg')] xs:bg-position-[center_top_23rem] sm:bg-position-[center_top_37rem] lg:bg-[url('../public/bible-edit.jpg')] bg-cover md:h-70 md:bg-position-[center_top_49rem] lg:bg-position-[center_top_30rem] lg:h-100 xl:bg-position-[center_top_45rem] 2xl:bg-position-[center_top_48rem]">
+            <h1 className="text-white text-5xl md:text-6xl lg:text-8xl text-shadow-lg font-noto-sans p-3 rounded-xl">
+                Media
+            </h1>
+        </div>
+
+    </div>
+  )
+}
