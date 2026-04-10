1ca9d2c3e5ba178c27addace5b46b256c92a6f66
Annika Budean <annikabudean@MacBook-Pro.local>
2026-04-10 16:35:08 -0700

    Changes to homepage - trying out ideas for the homepage sentence

--- src/fe/components/footer.tsx
+++ src/fe/components/footer.tsx
@@ -9,7 +9,7 @@ import { IoLocationOutline, IoLogoYoutube, IoMailOutline } from "react-icons/io5
 export default function Footer() {
     return (
       <footer className="flex flex-col md:items-center md:justify-center bg-zinc-700 text-white mt-auto">
-                
+            
             <div className="flex flex-col gap-10 pt-7 pl-5 pb-10 md:flex-row md:gap-20 ">
                 <div className="hidden md:flex md:flex-col">
                     <AFCLogo width={175} height={175}/>
