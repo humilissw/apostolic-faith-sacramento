57ab7e4226321a9f976a7663f848e8678683d3a6
cr1pto <20729543+cr1pto@users.noreply.github.com>
2026-04-11 21:46:23 -0700

    Fixed more async/await session over asyncsession issues

--- src/be/app/api/routes/users.py
+++ src/be/app/api/routes/users.py
@@ -62,7 +62,7 @@ async def create_user(*, session: SessionDep, user_in: UserCreate) -> Any:
             detail="The user with this email already exists in the system.",
         )
 
-    user = crud.create_user(session=session, user_create=user_in)
+    user = await crud.create_user(session=session, user_create=user_in)
     if settings.emails_enabled and user_in.email:
         email_data = generate_new_account_email(
             email_to=user_in.email, username=user_in.email, password=user_in.password
