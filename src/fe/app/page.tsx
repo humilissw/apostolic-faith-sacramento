d11f38e1750af4ef491b0135b56a3d56cff8dfe7
Annika Budean <annikabudean@MacBook-Pro.local>
2026-04-16 16:09:44 -0700

    Working responsive design for home page

--- src/fe/app/page.tsx
+++ src/fe/app/page.tsx
@@ -9,38 +9,38 @@ import HomepageParagraph from "@/components/homepage-paragraph"
 export default function Home() {
   return (
     <div className="">
-    <div className="w-full flex justify-start relative min-h-screen pb-20 sm:pb-32 md:pb-40 before:content-[''] before:absolute before:inset-0 before:bg-[url('../public/option2-copy.jpg')] before:bg-cover before:bg-center before:bg-fixed before:opacity-90 before:z-[-2] after:content-[''] after:absolute after:inset-0 after:bg-black/40 after:z-[-1]">
-      <div className="font-noto-sans xs:text-center lg:text-left text-shadow-lg/20 pb-16 pt-32 sm:pt-40 md:pt-48 lg:pt-56 pl-5 pr-5 sm:pl-10 md:pl-16 lg:pl-24">
-      <div className="space-y-4 font-light">
-        <h1 className="text-4xl text-white md:text-5xl lg:text-6xl tracking-wider ">
-          WELCOME TO 
+    <div className="w-full flex justify-center xl:justify-start relative min-h-[100dvh] pb-20 sm:pb-32 md:pb-40 before:content-[''] before:absolute before:inset-0 before:bg-[url('../public/option2-copy.jpg')] before:bg-cover before:bg-center before:bg-fixed before:opacity-90 before:z-[-2] after:content-[''] after:absolute after:inset-0 after:bg-black/48 after:z-[-1]">
+      <div className="font-noto-sans xs:text-center xl:text-left text-shadow-lg/20 pb-16 xs:pt-20 lg:pt-56 lg:pl-24">
+      <div className="font-light px-3">
+        <h1 className="leading-none pb-4 text-4xl md:text-5xl text-white lg:text-6xl tracking-wider ">
+          WELCOME TO THE
         </h1>
-        <h1 className="text-4xl md:text-5xl lg:text-6xl text-white tracking-wider ">
+        <h1 className="leading-none pb-4 text-4xl md:text-5xl lg:text-6xl text-white tracking-wider ">
            APOSTOLIC FAITH CHURCH
         </h1>
-        <h1 className="text-2xl text-white md:text-3xl lg:text-4xl tracking-wider">
+        <h1 className="text-2xl text-white lg:text-4xl tracking-wider">
            Sundays at 11:00 am & 5:00 pm
         </h1> 
         
       </div>
-        <div className="flex gap-8 xs:justify-center lg:justify-start pt-5 items-center">
+        <div className="flex gap-8 xs:justify-center xl:justify-start pt-5 items-center px-3">
           <Link href="/doctrines">
-              <Button className=" border-white bg-white/70 hover:text-white text-zinc-900" variant="default">About Us</Button>
+              <Button className="rounded-sm border-white bg-white/70 hover:text-white text-zinc-900" variant="default">About Us</Button>
           </Link>
           <Link href="https://www.youtube.com/@ApostolicFaithSacramento/streams">
-              <Button className=" border-white bg-white/70 hover:text-white text-zinc-900" size="default" variant="default">Latest Sermon</Button>
+              <Button className="rounded-sm border-white bg-white/70 hover:text-white text-zinc-900" size="default" variant="default">Latest Sermon</Button>
           </Link>
 
         </div>
       </div>
     </div>
-    <div className="flex flex-col md:flex-row justify-center items-center gap-8 md:gap-20 bg-white px-6 md:px-16 py-12 md:py-15 rounded-t-2xl -mt-8">
+    <div className="flex flex-col md:flex-row justify-center items-center gap-8 md:gap-20 bg-white px-6 md:px-16 py-12 md:py-15 -mt-8">
       <div className="w-full md:w-2xl">
         <p className="font-noto-sans text-3xl sm:text-4xl md:text-5xl font-light pb-5">Welcome!</p>
         <HomepageParagraph />
       </div>
       <div className="w-full md:w-auto flex md:block justify-center">
-        <Button className="bg-[#AA830E]">Learn More</Button>
+        <Button className="rounded-sm h-10 bg-[#C4921A] hover:bg-[#9A7214]">Learn More</Button>
       </div>
     </div>
     </div>
