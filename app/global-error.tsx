cb0ae2e79d401062a85d64b9e73077dbc63e85ae
cr1pto <20729543+cr1pto@users.noreply.github.com>
2026-06-04 09:21:01 -0700

    issues

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function GlobalError({}: {
+export default function GlobalError({ error, reset }: {
   error: Error & { digest?: string };
   reset: () => void;
 }) {
