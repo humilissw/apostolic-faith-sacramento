86defc6160c86f4f95790bff94e9eed823b82ae0
cr1pto <20729543+cr1pto@users.noreply.github.com>
2026-06-03 21:26:46 -0700

    turbopack

--- next.config.js
+++ next.config.js
@@ -14,6 +14,9 @@ const nextConfig = {
 
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
+  experimental: {
+    turbo: undefined
+  }
 }
 
 module.exports = nextConfig
