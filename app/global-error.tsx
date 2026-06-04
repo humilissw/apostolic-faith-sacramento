a3ccc4330ba983ef7449170e332b48d586247180
cr1pto <20729543+cr1pto@users.noreply.github.com>
2026-06-03 22:20:17 -0700

    issues

---
+++ app/global-error.tsx
@@ -0,0 +1,13 @@
+export default function GlobalError({}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  );
+}
