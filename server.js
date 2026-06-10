1622bbfa2cc45f27601522be7a7fd71f7af74eb7
cr1pto <20729543+cr1pto@users.noreply.github.com>
2026-06-10 14:30:05 -0700

    godaddy support

---
+++ server.js
@@ -0,0 +1,3 @@
+// Next.js app served via `next start` (defined in package.json scripts.start).
+// This file exists solely for GoDaddy Node.js Hosting contract.C2 / validator E008.
+module.exports = {};
