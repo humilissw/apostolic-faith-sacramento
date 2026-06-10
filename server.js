a6f518bf5f7d6abfd644ed686169e29bc9d97e6a
cr1pto <20729543+cr1pto@users.noreply.github.com>
2026-06-10 15:08:29 -0700

    Update output type export to standalone

--- server.js
+++
@@ -1,3 +0,0 @@
-// Next.js app served via `next start` (defined in package.json scripts.start).
-// This file exists solely for GoDaddy Node.js Hosting contract.C2 / validator E008.
-module.exports = {};
