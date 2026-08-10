573c50f66672ad23bf0fc97323d3bd9b7bf811dc
Annika Budean <184989881+annikabudean@users.noreply.github.com>
2026-08-10 11:58:08 -0700

    Changed footer to the no background logo

--- src/fe/components/afc-logo-white.tsx
+++ src/fe/components/afc-logo-white.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 
-export default function AFCLogoGrey(props: { width: number; height: number; }) {
+export default function AFCLogoWhite(props: { width: number; height: number; }) {
     return (
         <Link href="/" className="shrink-0">
             <Image
-            src='/logo-grey.jpg'
+            src='/logo-white.jpg'
             alt="Apostolic Faith Church Logo"
             width={props.width}
             height={props.height}/>
